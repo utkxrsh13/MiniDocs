124f485b8d00d176324010f95009f7d29d49418a
Utkarsh Tiwari <utkarsht0813@gmail.com>
2025-01-13 07:38:01 +0530

    final

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -10,22 +10,22 @@ function Card({data, reference}) {
   return (
     <motion.div drag dragConstraints={reference} whileDrag={{scale:1.2}} dragElastic={0.1} dragTransition={{ bounceStiffness: 600, bounceDamping: 10}} className='relative flex-shrink-0 w-60 h-72 rounded-[30px] bg-zinc-900/70 text-white py-8 px-5 overflow-hidden'>
       <FaRegFileAlt/>
-      <p className='text-sm leading-tight font-semibold mt-5'>{data.description}</p>
+      <p className='text-red-500 text-lg leading-tight font-semibold mt-5'>{data}</p>
 
       <div className='footer absolute bottom-0 left-0 w-full'>
         <div className='flex items-center justify-between mb-2 py-3 px-8'>
-          <h5>{data.filesize}</h5>
+          {/* <h5>{data.filesize}</h5> */}
           <span className='w-7 h-6 bg-zinc-600 rounded-full flex items-center justify-center'>
-            {data.close? <IoIosCloseCircle size="1em" color='white'/>:<LuDownload size=".7em" color='black'/>}
+            {/* {data.close? <IoIosCloseCircle size="1em" color='white'/>:<LuDownload size=".7em" color='black'/>} */}
             
             </span>
         </div>
 
-        {data.tag.isOpen && (
+        {/* {data.tag.isOpen && (
             <div className={`tag w-full py-4 ${data.tag.tagColor ==="blue"? "bg-blue-600":"bg-green-600"} flex justify-center items-center`}>
             <h3 className='text-sm font-semibold'>{data.tag.tagtitle}</h3>
           </div>
-        )}  
+        )}   */}
         
 
       </div>
--- src/Components/Foreground.jsx
+++ src/Components/Foreground.jsx
@@ -1,36 +1,61 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import Card from './Card'
+import { BackgroundBeamsWithCollision } from './BgwithBeams';
 
 function Foreground() {
   const ref = useRef(null)
+  const [text, setText] = useState("");
+  const [data, setData] = useState([]);
 
-  const data =[ 
-    {
-    description: "This is the color of the background",
-    filesize: ".6mb",
-    close: true,
-    tag:{isOpen: true, tagtitle: "Download Now", tagColor:"green"},
-  },
-    {
-    description: "This is the color of the background",
-    filesize: ".6mb",
-    close: true,
-    tag:{isOpen: false, tagtitle: "Download Now", tagColor:"green"},
-  },
-    {
-    description: "This is the color of the background",
-    filesize: ".6mb",
-    close: true,
-    tag:{isOpen: true, tagtitle: "load Now", tagColor:"blue"},
-  },
-];
+  const handleAddCard = () => {
+    if (text.trim() !== "") {
+      setData([...data, text]);
+      setText("");
+    }
+  };
+
+  // const data = [
+  //   {
+  //     description: "This is the color of the background",
+  //     filesize: ".6mb",
+  //     close: true,
+  //     tag: { isOpen: true, tagtitle: "Download Now", tagColor: "green" },
+  //   },
+  //   {
+  //     description: "This is the color of the background",
+  //     filesize: ".6mb",
+  //     close: true,
+  //     tag: { isOpen: true, tagtitle: "Download Now", tagColor: "green" },
+  //   },
+  //   {
+  //     description: "This is the color of the background",
+  //     filesize: ".6mb",
+  //     close: true,
+  //     tag: { isOpen: true, tagtitle: "load Now", tagColor: "blue" },
+  //   },
+  // ];
 
   return (
-    <div ref = {ref} className='w-full h-full fixed z-[3] top-0 left-0 flex flex-wrap gap-10 p-5'>
-      {data.map((item, index)=>(
-        <Card data = {item} reference = {ref}/>
-      ))}
-    </div>
+    <>
+      <BackgroundBeamsWithCollision
+      />
+      <div ref={ref} className='w-full h-full fixed z-[3] top-0 left-0 flex flex-wrap gap-10 p-5'>
+        <div>
+        <input
+        className='px-2 text-black font-semibold text-xl'
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Enter text"
+        />
+        <button className='text-white border ml-2 px-2 font-semibold' onClick={handleAddCard}>Add Card</button>
+        </div>
+        {data.map((item, index) => (
+          <Card data={item} key={index} reference={ref} />
+        ))}
+      </div>
+
+    </>
   )
 }
 
